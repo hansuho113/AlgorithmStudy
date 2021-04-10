37433bd9cdb380ec30083e515054c6d1c08c7d87
hansuho113 <hansuho113@naver.com>
2021-04-10 23:34:39 +0900

    21.04.10 카드2

---
+++ BaekJoon/class2/04_2164.py
@@ -0,0 +1,25 @@
+num = int(input())
+from collections import deque
+
+cards = deque(range(1, num+1))
+
+while len(cards) > 1:
+    cards.popleft()
+    cards.append(cards.popleft())
+
+print(cards[0])
+
+
+
+
+
+### 시간초과
+# cards = [i for i in range(1, num+1)]
+#
+# while True:
+#     if len(cards) != 1:
+#         cards.pop(0)
+#         cards.append(cards.pop(0))
+#     else:
+#         print(cards[0])
+#         break
