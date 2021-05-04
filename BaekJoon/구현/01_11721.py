c506d78cc8a82bc249d9cc612ed4d5a5dd3f75be
hansuho113 <hansuho113@naver.com>
2021-05-04 17:06:08 +0900

    21.05.04 열 개씩 끊어 출력하기

--- BaekJoon/구현/01_11721.py
+++ BaekJoon/구현/01_11721.py
@@ -12,5 +12,5 @@ else:
         print(s[i*10:(i*10+10)])    # 십의 자리수 만큼 반복하면서 10자리씩 출력
 
     if part_line != 0:
-        print(s[(full_line)*10:(full_line+part_line)])    # 남은 일의 자리가 0이 아니라면 십의 자리 반복한 것 이후부터 출력
+        print(s[(full_line)*10:(full_line*10+part_line)])    # 남은 일의 자리가 0이 아니라면 십의 자리 반복한 것 이후부터 출력
 
