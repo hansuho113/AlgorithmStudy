059b4828ee54ed8da7018f933f5bfc7abf6110e0
hansuho113 <hansuho113@naver.com>
2021-05-21 22:37:33 +0900

    21.05.21

--- CodingTest Lecture/01_greedy/03_multiply_or_sum.py
+++ CodingTest Lecture/01_greedy/03_multiply_or_sum.py
@@ -1,18 +1,32 @@
-import time
+# import time
+#
+# num_list = list(map(int, input()))
+#
+#
+# if num_list[0] and num_list[1] == 0 or 1:
+#     start_num = num_list[0] + num_list[1]
+# else: start_num = num_list[0] * num_list[1]
+#
+#
+# for num in num_list[2:]:
+#     if num == (0 or 1):
+#         start_num += num
+#     else:
+#         start_num *= num
+#
+# print(start_num)
+# e = time.time()
 
-num_list = list(map(int, input()))
 
+data = input()
 
-if num_list[0] and num_list[1] == 0 or 1:
-    start_num = num_list[0] + num_list[1]
-else: start_num = num_list[0] * num_list[1]
+result = int(data[0])
 
-
-for num in num_list[2:]:
-    if num == (0 or 1):
-        start_num += num
+for i in range(1, len(data)):
+    num = int(data[i])
+    if (num <= 1) or (result <= 1):
+        result += num
     else:
-        start_num *= num
+        result *= num
 
-print(start_num)
-e = time.time()
+print(result)
