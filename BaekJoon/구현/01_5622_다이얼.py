c3bb98f8621332200ecf18249642b7e2b2979e7a
hansuho113 <hansuho113@naver.com>
2021-06-01 22:43:17 +0900

    21.06.01 구현-다이얼

---
+++ BaekJoon/구현/01_5622_다이얼.py
@@ -0,0 +1,13 @@
+data = list(input())
+
+number_dict = {2: ["A", "B", "C"], 3: ["D", "E", "F"], 4: ["G", "H", "I"],
+               5: ["J", "K", "L"], 6: ["M", "N", "O"], 7: ["P", "Q", "R", "S"],
+               8: ["T", "U", "V"], 9: ["W", "X", "Y", "Z"]}
+
+time = 0
+for a in data:
+    for k, v in number_dict.items():
+        if a in v:
+            time += k+1
+
+print(time)
